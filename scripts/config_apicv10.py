e766b33a0ce7431c02bea5fd738f27a6c947001d
Joël Hartmann <145457540+hardi380@users.noreply.github.com>
2025-11-26 11:31:58 -0600

    Eliminate unnecessary steps in config_apicv10.py

--- scripts/config_apicv10.py
+++ scripts/config_apicv10.py
@@ -147,201 +147,8 @@ try:
     if response.status_code != 200:
           raise Exception("Return code for Sender and Email Server configuration isn't 200. It is " + str(response.status_code))
 
-#################################################
-# Step 6 - Register the Default Gateway Service #
-#################################################
-
-    print(info(6) + "#################################################")
-    print(info(6) + "# Step 6 - Register the Default Gateway Service #")
-    print(info(6) + "#################################################")
-
-    # First, we need to get the Datapower API Gateway instances details
-
-    url = 'https://' + environment_config["APIC_ADMIN_URL"] + '/api/cloud/integrations/gateway-service/datapower-api-gateway'
-
-    response = api_calls.make_api_call(url, admin_bearer_token, 'get')
-
-    if response.status_code != 200:
-          raise Exception("Return code for getting the Datapower API Gateway instances details isn't 200. It is " + str(response.status_code))
-
-    datapower_api_gateway_url = response.json()['url']
-    if DEBUG:
-        print(info(6) + "Email Server url: " + datapower_api_gateway_url)
-
-    # Second, we need to get the TLS server profiles
-
-    url = 'https://' + environment_config["APIC_ADMIN_URL"] + '/api/orgs/' + admin_org_id + '/tls-server-profiles'
-
-    response = api_calls.make_api_call(url, admin_bearer_token, 'get')
-
-    found = False
-    tls_server_profile_url = ''
-    if response.status_code != 200:
-          raise Exception("Return code for getting the TLS server profiles isn't 200. It is " + str(response.status_code))
-    for profile in response.json()['results']:
-        if profile['name'] == "tls-server-profile-default":
-            found = True
-            tls_server_profile_url = profile['url']
-    if not found:
-        raise Exception("[ERROR] - The default TLS server profile was not found in the IBM API Connect Cluster instance")
-
-    if DEBUG:
-        print(info(6) + "Default TLS server profile url: " + tls_server_profile_url)
-
-    # Third, we need to get the TLS client profiles
-
-    url = 'https://' + environment_config["APIC_ADMIN_URL"] + '/api/orgs/' + admin_org_id + '/tls-client-profiles'
-
-    response = api_calls.make_api_call(url, admin_bearer_token, 'get')
-
-    found = False
-    tls_client_profile_url = ''
-    if response.status_code != 200:
-          raise Exception("Return code for getting the TLS client profiles isn't 200. It is " + str(response.status_code))
-    for profile in response.json()['results']:
-        if profile['name'] == "gateway-management-client-default":
-            found = True
-            tls_client_profile_url = profile['url']
-    if not found:
-        raise Exception("[ERROR] - The Gateway Management TLS client profile was not found in the IBM API Connect Cluster instance")
-
-    if DEBUG:
-        print(info(6) + "Gateway Management TLS server profile url: " + tls_client_profile_url)
-
-
-    # Finally, we can actually make the REST call to get the Default Gateway Service registered
-
-    url = 'https://' + environment_config["APIC_ADMIN_URL"] + '/api/orgs/' + admin_org_id + '/availability-zones/custom-availability-zone/gateway-services'
-    
-    # Create the data object
-    data = {}
-    data['name'] = "default-gateway-service"
-    data['title'] = "Default Gateway Service"
-    data['summary'] = "Default Gateway Service that comes out of the box with API Connect Cluster v10"
-    data['endpoint'] = 'https://' + environment_config["APIC_GATEWAY_MANAGER_URL"]
-    data['api_endpoint_base'] = 'https://' + environment_config["APIC_GATEWAY_URL"]
-    data['tls_client_profile_url'] = tls_client_profile_url
-    data['gateway_service_type'] = 'datapower-api-gateway'
-    visibility = {}
-    visibility['type'] = 'public'
-    data['visibility'] = visibility
-    sni = []
-    sni_inner={}
-    sni_inner['host'] = '*'
-    sni_inner['tls_server_profile_url'] = tls_server_profile_url
-    sni.append(sni_inner)
-    data['sni'] = sni
-    data['integration_url'] = datapower_api_gateway_url
-
-    if DEBUG:
-        print(info(6) + "This is the data object:")
-        print(info(6), data)
-        print(info(6) + "This is the JSON dump:")
-        print(info(6), json.dumps(data))
-
-    response = api_calls.make_api_call(url, admin_bearer_token, 'post', data)
-
-    if response.status_code != 201:
-          raise Exception("Return code for registering the Default Gateway Service isn't 201. It is " + str(response.status_code))
-
-    # This will be needed in the last step when we associate this Gateway Service to the Sandbox catalog
-    gateway_service_id = response.json()['id']
-    if DEBUG:
-        print(info(6) + "Default Gateway Service ID: " + gateway_service_id)
-
-###################################################
-# Step 7 - Register the Default Analytics Service #
-###################################################
-
-    print(info(7) + "###################################################")
-    print(info(7) + "# Step 7 - Register the Default Analytics Service #")
-    print(info(7) + "###################################################")
-
-    url = 'https://' + environment_config["APIC_ADMIN_URL"] + '/api/orgs/' + admin_org_id + '/availability-zones/custom-availability-zone/analytics-services'
-    
-    # Create the data object
-    data = {}
-    data['name'] = "default-analytics-service"
-    data['title'] = "Default Analytics Service"
-    data['summary'] = "Default Analytics Service that comes out of the box with API Connect Cluster v10"
-    data['endpoint'] = 'https://' + environment_config["APIC_ANALYTICS_CONSOLE_URL"]
-
-    if DEBUG:
-        print(info(7) + "This is the data object:")
-        print(info(7), data)
-        print(info(7) + "This is the JSON dump:")
-        print(info(7), json.dumps(data))
-
-    response = api_calls.make_api_call(url, admin_bearer_token, 'post', data)
-
-    if response.status_code != 201:
-          raise Exception("Return code for registering the Default Analytics Service isn't 201. It is " + str(response.status_code))
-
-    analytics_service_url = response.json()['url']
-    if DEBUG:
-        print(info(6) + "Default Analytics Service url: " + analytics_service_url)
-
-#############################################################################
-# Step 8 - Associate Default Analytics Service with Default Gateway Service #
-#############################################################################
-
-    print(info(8) + "#############################################################################")
-    print(info(8) + "# Step 8 - Associate Default Analytics Service with Default Gateway Service #")
-    print(info(8) + "#############################################################################")
-
-    url = 'https://' + environment_config["APIC_ADMIN_URL"] + '/api/orgs/' + admin_org_id + '/availability-zones/custom-availability-zone/gateway-services/default-gateway-service'
-    
-    # Create the data object
-    data = {}
-    data['analytics_service_url'] = analytics_service_url
-
-    if DEBUG:
-        print(info(8) + "This is the data object:")
-        print(info(8), data)
-        print(info(8) + "This is the JSON dump:")
-        print(info(8), json.dumps(data))
-
-    response = api_calls.make_api_call(url, admin_bearer_token, 'patch', data)
-
-    if response.status_code != 200:
-          raise Exception("Return code for associating the Default Analytics Service with the Default Gateway Service isn't 200. It is " + str(response.status_code))
-
-################################################
-# Step 9 - Register the Default Portal Service #
-################################################
-
-    print(info(9) + "################################################")
-    print(info(9) + "# Step 9 - Register the Default Portal Service #")
-    print(info(9) + "################################################")
-
-    url = 'https://' + environment_config["APIC_ADMIN_URL"] + '/api/orgs/' + admin_org_id + '/availability-zones/custom-availability-zone/portal-services'
-    
-    # Create the data object
-    data = {}
-    data['title'] = "Default Portal Service"
-    data['name'] = "default-portal-service"
-    data['summary'] = "Default Portal Service that comes out of the box with API Connect Cluster v10"
-    data['endpoint'] = 'https://' + environment_config["APIC_PORTAL_DIRECTOR_URL"]
-    data['web_endpoint_base'] = 'https://' + environment_config["APIC_PORTAL_WEB_URL"]
-    visibility = {}
-    visibility['group_urls'] = None
-    visibility['org_urls'] = None
-    visibility['type'] = 'public'
-    data['visibility'] = visibility
-
-    if DEBUG:
-        print(info(9) + "This is the data object:")
-        print(info(9), data)
-        print(info(9) + "This is the JSON dump:")
-        print(info(9), json.dumps(data))
-
-    response = api_calls.make_api_call(url, admin_bearer_token, 'post', data)
-
-    if response.status_code != 201:
-          raise Exception("Return code for registering the Default Portal Service isn't 201. It is " + str(response.status_code))
-
 ############################################
-# Step 10 - Create a Provider Organization #
+# Step 6 - Create a Provider Organization #
 ############################################
 
     print(info(10) + "############################################")
@@ -417,7 +224,7 @@ try:
           raise Exception("Return code for creating the provider organization isn't 201. It is " + str(response.status_code))
 
 ###############################################################
-# Step 11 - Get the IBM API Connect Provider API Bearer Token #
+# Step 7 - Get the IBM API Connect Provider API Bearer Token #
 ###############################################################
 
     print(info(11) + "###############################################################")
@@ -439,7 +246,7 @@ try:
         print(info(11), admin_bearer_token)
 
 #########################################################################
-# Step 12 - Associate Default Gateway Services with the Sandbox catalog #
+# Step 8 - Associate Default Gateway Services with the Sandbox catalog #
 #########################################################################
 
     print(info(12) + "#########################################################################")
